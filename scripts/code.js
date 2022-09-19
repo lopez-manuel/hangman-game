0ee4372e98aa4b82e62502482d36abcbc8cffaf2
Lopez-manuel <lopez.ledezma.manuel@gmail.com>
2022-09-19 17:14:46 -0500

    se agrego boton de nueva palabra

--- scripts/code.js
+++ scripts/code.js
@@ -3,6 +3,7 @@ let option2 = document.getElementById("option2")
 let startGame = document.getElementById("start-game")
 let inputLetter = document.getElementById("input-01")
 let check = document.getElementById("check")
+let nextWord = document.getElementById("next-word")
 let welcomeScreen = document.getElementById("welcome")
 let categoriesScreen = document.getElementById("contenedor-categories")
 let animalsCat = document.getElementById("animals-cat")
@@ -11,24 +12,22 @@ let disneyCat = document.getElementById("disney-cat")
 let pixartCat = document.getElementById("pixart-cat")
 let selectCategory = document.getElementById("select-category")
 let pcScreen = document.getElementById("pc-screen")
-let card1 = document.getElementById("card1")
-let card2 = document.getElementById("card2")
 let lettersOutContainer = document.getElementById("lettersout-container")
 let imgContainer = document.getElementById("img-container")
+let clueContainer = document.getElementById("clue-container")
 let idArray = []
 let word =""
 let strikes = 0
 let count = 0
 let x= 0
-let clueContainer = document.getElementById("clue-container")
+let gameOver = false
+let wordComplete = []
 
 
 
 
 
-
-
-
+//evento para seleccionar tipo de juego
 startGame.addEventListener("click", () =>{
 
     if(option1.checked){
@@ -51,6 +50,7 @@ startGame.addEventListener("click", () =>{
     }
 })
 
+//evento para seleccionar la categoria
 selectCategory.addEventListener("click", ()=>{
 
     if(animalsCat.checked){
@@ -58,6 +58,8 @@ selectCategory.addEventListener("click", ()=>{
         word = computer(animals)
         checkLetters()
         categoriesScreen.style.display = "none"
+        nextWord.style.display = "none"
+        check.style.display = "block"
         pcScreen.style.display = "grid"
 
     }
@@ -77,7 +79,7 @@ selectCategory.addEventListener("click", ()=>{
 
 })
 
-
+//evento que comprueba la letra introducida
 check.addEventListener("click", () =>{
 
     console.log("se disparo el evento")
@@ -102,16 +104,18 @@ check.addEventListener("click", () =>{
                 let zebra = document.getElementById(idArray[j])
                 console.log(zebra)
                 zebra.innerHTML = inputLetter.value.toUpperCase()
-                x++
                 count++
+                wordComplete[j] = inputLetter.value.toUpperCase()
+                x=checkWord()
 
-                if(count === word.length){
+
+                if(x === word.length){
 
                     alert("felicidades ganaste")
+                    nextGame()
                 }
 
                 else{
-
                 }
     
             }
@@ -125,13 +129,30 @@ check.addEventListener("click", () =>{
         if(x<1){
     
             strikes++
-            console.log("strike: "+strikes)
-            imgContainer.innerHTML = `<figure>
-            <img src="./images/ahorcado/${strikes+1}.png" alt="1">
+
+            if (strikes === 7)
+            {
+                console.log("strike: "+strikes)
+                imgContainer.innerHTML = `<figure>
+            <img src="./images/ahorcado/${strikes}.png" alt="1">
             </figure>`
-            inputLetter.value = ''
+                inputLetter.value = ''
+                alert("perdiste")
+                nextGame ()
+            }
+
+            else {
+                console.log("strike: "+strikes)
+                imgContainer.innerHTML = `<figure>
+            <img src="./images/ahorcado/${strikes}.png" alt="1">
+            </figure>`
+                inputLetter.value = ''
+
+            }
+
     
         }
+
     
         else{
     
@@ -145,28 +166,44 @@ check.addEventListener("click", () =>{
 
 })
 
+nextWord.addEventListener("click", () =>{
+
+    word=computer(animals)
+    checkLetters()
+    check.style.display = "block"
+    nextWord.style.display = "none"
+    strikes = 0
+    imgContainer.innerHTML = `<figure>
+            <img src="./images/ahorcado/0.png" alt="1">
+            </figure>`
+    inputLetter.value = ''
+})
 
 
+//funcion que escoge la palabra del arreglo de la categoria y almacena letra por letra en otro arreglo
 function computer (category){
-
 
     let palabra = ""
     let aleatorio = Math.floor(Math.random() * (category.length))
+    let array = []
 
-
-
-
-    palabra = category[aleatorio].name
+    palabra = category[aleatorio].name.toUpperCase()
 
     console.log(palabra)
     clueContainer.innerHTML = category[aleatorio].clue
 
-    return palabra
+    for (let i = 0; i < palabra.length; i++) {
+        array[i] = palabra[i]
+    }
+
+    return array
 
 }
 
-
+//funcion para generar los cuadros de las letras dependiendo de la palabra
 function checkLetters (){
+
+    lettersOutContainer.innerHTML = ""
 
     for (let j = 0; j< word.length; j++) {
       
@@ -181,4 +218,30 @@ function checkLetters (){
         idArray[j]=`out-${j}`
        
     }
+}
+//funcion para comprobar que todas las letras del arreglo se parezcan
+function checkWord (){
+    let allLetters = 0
+    for (let k = 0; k < word.length ; k++) {
+
+        if (word[k] === wordComplete[k]){
+            allLetters++
+        }
+
+        else{
+
+        }
+
+    }
+
+    return allLetters
+}
+//funcion que muestra el boton siguiente palabra
+function nextGame (){
+    check.style.display = "none"
+    nextWord.style.display = "block"
+    wordComplete = []
+
+
+
 }
